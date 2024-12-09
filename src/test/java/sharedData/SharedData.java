c6c48b0d356c0de097016f13e2a581256768e5ec
hammerheart92 <hammerheart92@gmail.com>
2024-12-09 17:08:38 +0200

    Ci/Cd implementation

--- src/test/java/sharedData/SharedData.java
+++ src/test/java/sharedData/SharedData.java
@@ -2,7 +2,6 @@ package sharedData;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 
 import java.time.Duration;
 
@@ -11,13 +10,7 @@ public class SharedData {
     private WebDriver driver;
 
     public void setUpDriver(){
-        EdgeOptions options = new EdgeOptions();
-        options.addArguments("--headless=new"); // Run in headless mode for CI
-        options.addArguments("--no-sandbox"); // Required for GitHub Actions
-        options.addArguments("--disable-dev-shm-usage"); // Prevents memory issues
-        options.addArguments("--window-size=1920,1080"); // Set a default window size
-
-        driver = new EdgeDriver(options);
+        driver = new EdgeDriver();
         driver.get("https://formy-project.herokuapp.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
