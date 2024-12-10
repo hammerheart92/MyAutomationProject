1d65071663d9128218f586d20d9beba085eabe22
hammerheart92 <hammerheart92@gmail.com>
2024-12-10 15:08:26 +0200

    Adding some extra logs for Complete Web From

--- src/test/java/pages/CompleteWebFormPage.java
+++ src/test/java/pages/CompleteWebFormPage.java
@@ -47,14 +47,17 @@ public class CompleteWebFormPage extends BasePage {
         elementMethods.fillElement(firstNameElement, testData.getFirstName());
         loggerUtility.infoLog("The user fills the First name field with " + testData.getFirstName() + " value");
         Assert.assertEquals(firstNameElement.getAttribute("value"),testData.getFirstName(),"First name value is not correct");
+        loggerUtility.infoLog("The user validates the presence of " + testData.getFirstName() + " value of the table content");
 
         elementMethods.fillElement(lastNameElement, testData.getLastName());
         loggerUtility.infoLog("The user fills the Last name field with " + testData.getLastName() + " value");
         Assert.assertEquals(lastNameElement.getAttribute("value"),testData.getLastName(),"Last name value is not correct");
+        loggerUtility.infoLog("The user validates the presence of " + testData.getLastName() + " value of the table content");
 
         elementMethods.fillElement(jobTitleElement, testData.getJobTitle());
         loggerUtility.infoLog("The user fills the Job title field with " + testData.getJobTitle() + " value");
         Assert.assertEquals(jobTitleElement.getAttribute("value"),testData.getJobTitle(),"Job tile value is not correct");
+        loggerUtility.infoLog("The user validates the presence of " + testData.getJobTitle() + " value of the table content");
 
         selectEducationLevel(testData.getEducationLevel());
         selectSex(testData.getSex());
@@ -63,13 +66,16 @@ public class CompleteWebFormPage extends BasePage {
         String selectedExperience = elementMethods.getSelectedDropdownOption(yearsOfExperienceElement);
         Assert.assertEquals(selectedExperience, testData.getYearsOfExperience(),
                 "Years of experience value is not correct!");
+        loggerUtility.infoLog("Assertion passed: Years of experience value is correct.");
 
-        // Send the date value directly to the date input field in the format MM/DD/YYYY
         String fullDate = testData.getMonth() + "/" + testData.getDay() + "/" + testData.getYear();
-        elementMethods.fillElement(dateElement, fullDate); // Input the date in the field
+        loggerUtility.infoLog("Constructed full date: " + fullDate);
+        elementMethods.fillElement(dateElement, fullDate);
+        loggerUtility.infoLog("The user inputs the date value: " + fullDate + " into the date field.");
 
         Assert.assertEquals(dateElement.getAttribute("value"), fullDate,
                 "Date value is not correct!");
+        loggerUtility.infoLog("Assertion passed: Date field value is correct.");
 
         loggerUtility.infoLog("The user selects the Date from the option list");
         elementMethods.waitVisibleElement(submitElement);
@@ -88,6 +94,7 @@ public class CompleteWebFormPage extends BasePage {
         Assert.assertEquals(confirmationMessage.getText(), "Thanks for submitting your form",
                 "The confirmation message is incorrect or not displayed.");
         loggerUtility.infoLog("The confirmation message validated successfully.");
+        loggerUtility.infoLog("Assertion passed: Confirmation message is validated successfully.");
 
     }
 
@@ -97,16 +104,19 @@ public class CompleteWebFormPage extends BasePage {
             case "high school":
                 elementMethods.clickElement(highSchoolElement);
                 Assert.assertTrue(highSchoolElement.isSelected(),"High School radio button is not selected!");
+                loggerUtility.infoLog("Assertion passed: High School radio button is selected.");
                 loggerUtility.infoLog("Selected High School.");
                 break;
             case "college":
                 elementMethods.clickElement(collegeElement);
                 Assert.assertTrue(collegeElement.isSelected(),"Collage radio button is not selected!");
+                loggerUtility.infoLog("Assertion passed: College radio button is selected.");
                 loggerUtility.infoLog("Selected Collage.");
                 break;
             case "grad school":
                 elementMethods.clickElement(gradSchoolElement);
                 Assert.assertTrue(gradSchoolElement.isSelected(),"Grad School radio button is not selected!");
+                loggerUtility.infoLog("Assertion passed: Grad School radio button is selected.");
                 loggerUtility.infoLog("Selected Grad School.");
                 break;
             default:
@@ -121,16 +131,19 @@ public class CompleteWebFormPage extends BasePage {
             case "male":
                 elementMethods.clickElement(maleElement);
                 Assert.assertTrue(maleElement.isSelected(),"Male checkbox is not selected!");
+                loggerUtility.infoLog("Assertion passed: Male checkbox is selected.");
                 loggerUtility.infoLog("Selected Male.");
                 break;
             case "female":
                 elementMethods.clickElement(femaleElement);
                 Assert.assertTrue(femaleElement.isSelected(),"Female checkbox is not selected!");
+                loggerUtility.infoLog("Assertion passed: Female checkbox is selected.");
                 loggerUtility.infoLog("Selected Female.");
                 break;
             case "prefer not to say":
                 elementMethods.clickElement(preferNotToSayElement);
                 Assert.assertTrue(preferNotToSayElement.isSelected(),"Prefer not to say checkbox is not selected!");
+                loggerUtility.infoLog("Assertion passed: Prefer Not to Say checkbox is selected.");
                 loggerUtility.infoLog("Selected Prefer Not to Say.");
                 break;
             default:
